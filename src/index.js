c18d1c680e9d5f9f87a25cb573107cab73246608
moise10r <moiserushanika2006@gmail.com>
2021-07-14 13:09:56 +0200

    Refactor the task list style.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { getTask } from './data';
 const taskList = document.querySelector('.task-list-container');
 
 getTask().forEach((task) => {
-  taskList.innerHTML += `<li class=" container task flex-center">
-  <span class="left">
+  taskList.innerHTML += `<li class="  task flex-center">
+  <span class="left flex-center">
     <input id=${task.index} type="checkbox" />
-    <input value=${task.description} class="edit-task">
+    <input value='${task.description}' class="edit-task">
   </span>
   <span class="right">
     <i class="fas fa-ellipsis-v"></i>
