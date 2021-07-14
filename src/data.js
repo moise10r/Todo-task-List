cc39ac7500d1db025dc525ea06523924a7f7410a
moise10r <moiserushanika2006@gmail.com>
2021-07-15 00:52:19 +0200

    Fix add task bug

--- src/data.js
+++ src/data.js
@@ -14,6 +14,7 @@ const addTaskForm = document.querySelector('#add-task');
 const data = JSON.parse(localStorage.getItem('Task-list')) || [];
 addTaskForm.addEventListener('submit', () => {
   const inputTaskValue = document.querySelector('#add-task-input').value;
+  if (inputTaskValue === '') return;
   const newTask = new Task(inputTaskValue);
   data.push(newTask);
   localStorage.setItem('Task-list', JSON.stringify(data));
