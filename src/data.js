c8aae909a8c71f7f67ec47d2761434f492f2ff3c
moise10r <moiserushanika2006@gmail.com>
2021-07-15 13:46:36 +0200

    Implement the add task opertaion

--- src/data.js
+++ src/data.js
@@ -1,26 +1,8 @@
-/* eslint-disable no-restricted-globals */
-/* eslint-disable no-alert */
 /* eslint-disable import/prefer-default-export */
+/* eslint-disable import/no-cycle */
+import { addTask } from './updateTask';
 
-class Task {
-  constructor(description) {
-    this.description = description;
-    this.index = new Date();
-    this.completed = false;
-  }
-}
-
-const addTaskForm = document.querySelector('#add-task');
-const data = JSON.parse(localStorage.getItem('Task-list')) || [];
-addTaskForm.addEventListener('submit', () => {
-  const inputTaskValue = document.querySelector('#add-task-input').value;
-  if (inputTaskValue === '') return;
-  const newTask = new Task(inputTaskValue);
-  data.push(newTask);
-  localStorage.setItem('Task-list', JSON.stringify(data));
-});
-
-// import { addTask } from './updateTask';
+const data = addTask();
 export function getTask() {
   return data.map((task) => task);
 }
--- src/updateTask.js
+++ src/updateTask.js
@@ -39,3 +39,23 @@ export function deleteTask() {
     }
   });
 }
+
+export function addTask() {
+  class Task {
+    constructor(description) {
+      this.description = description;
+      this.index = new Date();
+      this.completed = false;
+    }
+  }
+  const addTaskForm = document.querySelector('#add-task');
+  const data = JSON.parse(localStorage.getItem('Task-list')) || [];
+  addTaskForm.addEventListener('submit', () => {
+    const inputTaskValue = document.querySelector('#add-task-input').value;
+    if (inputTaskValue === '') return;
+    const newTask = new Task(inputTaskValue);
+    data.push(newTask);
+    localStorage.setItem('Task-list', JSON.stringify(data));
+  });
+  return data;
+}
